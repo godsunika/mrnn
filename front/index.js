17359d83f8e88aea83a862690cfdb520e045e72c
godsunika <103250935+godsunika@users.noreply.github.com>
2022-05-12 11:29:29 +0700

    Add files via upload

---
+++ front/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
+import rootReducer from './store/reducers/rootReducer'
+import setupInterceptors from "./api/setupInterceptors";
+
+const store = createStore(rootReducer, compose(applyMiddleware(thunk)))
+
+setupInterceptors(store);
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Provider store = { store }>
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
